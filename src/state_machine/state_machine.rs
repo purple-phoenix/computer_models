1919f0fcbfab1e48f7c88d38c17f1a9956672d77
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-06 18:21:52 -0500

    Match bit and stub state mapping add helper car_cadr and test

--- src/state_machine/state_machine.rs
+++ src/state_machine/state_machine.rs
@@ -1,3 +1,5 @@
+use crate::state_machine::state_machine::State::{S0, S1, S2};
+
 #[derive(Debug, PartialOrd, PartialEq)]
 enum AutomatonAcceptance {
     Accept,
@@ -11,11 +13,22 @@ enum State {
     S2
 }
 
-fn fsm(str: String) -> AutomatonAcceptance {
+fn fsm(str: &str) -> AutomatonAcceptance {
     return fsm_aux(str, State::S0)
 }
 
-fn fsm_aux(str: String, state: State) -> AutomatonAcceptance {
+fn fsm_aux(str: &str, state: State) -> AutomatonAcceptance {
+    if str.is_empty() {
+        return determine_state_acceptance(state)
+    }
+    else {
+        let bit = str.chars().next().unwrap();
+        return match state {
+             S0 => AutomatonAcceptance::Reject,
+             S1 => AutomatonAcceptance::Reject,
+             S2 => AutomatonAcceptance::Reject
+        }
+    }
     return AutomatonAcceptance::Reject
 }
 
@@ -28,6 +41,13 @@ fn determine_state_acceptance(state: State) -> AutomatonAcceptance {
     }
 }
 
+fn car_cadr(s: &str) -> (&str, &str) {
+    match s.chars().next() {
+        Some(c) => s.split_at(c.len_utf8()),
+        None => s.split_at(0),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -37,5 +57,13 @@ mod tests {
         assert_eq!(AutomatonAcceptance::Accept, determine_state_acceptance(State::S0));
         assert_eq!(AutomatonAcceptance::Reject, determine_state_acceptance(State::S1));
         assert_eq!(AutomatonAcceptance::Reject, determine_state_acceptance(State::S2));
+    }
+
+    #[test]
+    fn test_car_cadr() {
+        let some_str = "some_str";
+        let empty_str = "";
+        assert_eq!(("s", "ome_str"), car_cadr(some_str));
+        assert_eq!(("", ""), car_cadr(empty_str));
     }
 }
