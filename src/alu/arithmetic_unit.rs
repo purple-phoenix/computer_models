7e47941eeb40cc830160fdb04f82ef5f547390cf
Matt McCarthy <matttmccarthy66@gmail.com>
2019-11-08 20:26:02 -0500

    Implement full adder

--- src/alu/arithmetic_unit.rs
+++ src/alu/arithmetic_unit.rs
@@ -9,6 +9,20 @@ pub fn make_half_adder() -> fn(&MBoolean, &MBoolean) -> (MBoolean, MBoolean){
     }
 }
 
+pub fn make_full_adder() -> fn(&MBoolean, &MBoolean, &MBoolean) -> (MBoolean, MBoolean) {
+    |inputA, inputB, inputC| {
+        let half_add_ab = make_half_adder()(inputA, inputB);
+        let sum_ab = half_add_ab.0;
+        let carry_ab = half_add_ab.1;
+        let sum_carry_abc = make_half_adder()(&sum_ab, inputC);
+        let total_sum = sum_carry_abc.0;
+        let carry_abc = sum_carry_abc.1;
+        let total_carry = make_or()(&carry_ab, &carry_abc);
+        return (total_sum, total_carry)
+    }
+
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -27,5 +41,41 @@ mod test {
         //1 + 1 = 0 and carry
         assert_eq!((MBoolean::FALSE, MBoolean::TRUE),
                 make_half_adder()(&MBoolean::TRUE, &MBoolean::TRUE));
+    }
+
+    #[test]
+    fn test_full_adder() {
+        //0 + 0 + 0 = 0 no carry
+        assert_eq!((MBoolean::FALSE, MBoolean::FALSE),
+            make_full_adder()(&MBoolean::FALSE, &MBoolean::FALSE, &MBoolean::FALSE)
+        );
+
+        //0 + 0 + 1 = 1 no carry
+        assert_eq!((MBoolean::TRUE, MBoolean::FALSE),
+                   make_full_adder()(&MBoolean::FALSE, &MBoolean::FALSE, &MBoolean::TRUE));
+
+        //0 + 1 + 0 = 1 no carry
+        assert_eq!((MBoolean::TRUE, MBoolean::FALSE),
+                   make_full_adder()(&MBoolean::FALSE, &MBoolean::TRUE, &MBoolean::FALSE));
+
+        //1 + 0 + 0 = 1 no carry
+        assert_eq!((MBoolean::TRUE, MBoolean::FALSE),
+                   make_full_adder()(&MBoolean::TRUE, &MBoolean::FALSE, &MBoolean::FALSE));
+
+        //0 + 1 + 1 = 0 and carry
+        assert_eq!((MBoolean::FALSE, MBoolean::TRUE),
+                   make_full_adder()(&MBoolean::FALSE, &MBoolean::TRUE, &MBoolean::TRUE));
+
+        //1 + 0 + 1 = 0 and carry
+        assert_eq!((MBoolean::FALSE, MBoolean::TRUE),
+                   make_full_adder()(&MBoolean::TRUE, &MBoolean::FALSE, &MBoolean::TRUE));
+
+        //1 + 1 + 0 = 0 and carry
+        assert_eq!((MBoolean::FALSE, MBoolean::TRUE),
+                   make_full_adder()(&MBoolean::TRUE, &MBoolean::TRUE, &MBoolean::FALSE));
+
+        //1 + 1 + 1 = 1 and carry
+        assert_eq!((MBoolean::TRUE, MBoolean::TRUE),
+                   make_full_adder()(&MBoolean::TRUE, &MBoolean::TRUE, &MBoolean::TRUE));
     }
 }
